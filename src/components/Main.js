82fc261ead32f111dc1a8c0d58ccc627d9cc125d
Laure Claret <laure.a.claret@gmail.com>
2020-07-02 16:58:54 +1000

    message

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Main(props) {
     return (
         <div className='main'>
-            <h1>I'm a Web Developper living in Brisbane.</h1>
+            <h1>I'm a Web Developer living in Brisbane.</h1>
         </div>
     );
 }
